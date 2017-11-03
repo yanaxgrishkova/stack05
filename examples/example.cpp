a2341191729aad0f0624aa4d508d04b3b26a371d
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-11-03 20:36:44 +0300

    Update example.cpp

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,22 +1,45 @@
-#include <iostream>
-#include <sstream>
-
 #include "stack.hpp"
+#include <thread>
+#include <chrono>
+#include <thread>
+
+void producer(stack<int>& _stack)
+{	
+	unsigned int i = 0;
+	for (;;)
+	{
+		_stack.push(i);
+		++i;
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+
+void consumer(stack<int>& _stack)
+{
+	for (;;)
+	{
+		try
+		{
+			_stack.pop();
+		}
+		catch (...)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	}
+}
+
+
 
 int main()
 {
-    stack<int> s;
-    for( std::string line; std::getline( std::cin, line ); ) {
-        std::istringstream stream{ line };
-        if( char op; stream >> op ) {
-            if( op == 'e' ) {
-                std::cout << std::boolalpha << s.empty();
-            } 
-            else if( op == 'q' ) {
-                break;
-            }
-        }
-    }
-    
-    return 0;
+	stack<int> s1;
+	std::thread prod(producer, std::ref(s1));
+	std::thread cons(consumer, std::ref(s1));
+
+	prod.join();
+	cons.join();
+
+	return 0;
 }
