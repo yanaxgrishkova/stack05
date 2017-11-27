28a5a1eee8d51f3e354a564191e638ef6cd72d2a
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-11-27 10:37:55 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -2,12 +2,9 @@
 #include <stdexcept>
 #include <algorithm>
 #include <utility>
-#include <thread>
-#include <new>
 #include <mutex>
-#include <condition_variable>
 
-template <typename T> /*thread-safe*/
+template <typename T>
 class stack
 {
 private:
@@ -19,13 +16,11 @@ public:
 	stack<T>& operator = (stack<T> const &);
 	size_t count() const;
 	void push(T const &);
-	auto try_pop() -> std::shared_ptr<T>;
-  	auto wait_and_pop() -> std::shared_ptr<T>;
+	auto pop() -> std::shared_ptr<T>;
 	bool empty() const;
 
 private:
 	mutable std::mutex mutex_;
-	std::condition_variable cond_;
 	T* array_;
 	size_t array_size_;
 	size_t count_;
@@ -39,7 +34,6 @@ template <typename T>
 size_t stack<T>::count() const
 {
 	std::lock_guard<std::mutex> lock(mutex_);
-	
 	return count_;
 }
 
@@ -47,12 +41,10 @@ template <typename T>
 stack<T>::stack(stack<T> const& copy)
 {
 	std::lock_guard<std::mutex> lock(copy.mutex_);
-	
 	T* temp = new T[copy.array_size_];
 	array_size_ = copy.array_size_;
 	count_ = copy.count_;
 	array_ = temp;
-	
 	try
 	{
 		std::copy(copy.array_, copy.array_ + count_, array_);
@@ -80,11 +72,9 @@ template <typename T>
 void stack<T>::swap(stack<T>& other)
 {
 	std::lock(mutex_, other.mutex_);
-	
 	std::swap(array_, other.array_);
 	std::swap(array_size_, other.array_size_);
 	std::swap(count_, other.count_);
-	
 	mutex_.unlock();
 	other.mutex_.unlock();
 }
@@ -92,10 +82,11 @@ void stack<T>::swap(stack<T>& other)
 template <typename T>
 void stack<T>::push(T const& value)
 {
-	std::unique_lock<std::mutex> lock(mutex_);
+	std::lock_guard<std::mutex> lock(mutex_);
 	if (array_size_ == count_)
 	{
-		size_t size = array_size_ == 0 ? 1 : array_size_ * 2;
+		size_t size = array_size_;
+		size == 0 ? 1 : array_size_ * 2;
 
 		T* temp = new T[size];
 		
@@ -103,6 +94,7 @@ void stack<T>::push(T const& value)
 		{
 			std::copy(array_, array_ + count_, temp);	
 		}
+		
 		catch ( ... )
 		{
 			delete[] temp;
@@ -116,39 +108,22 @@ void stack<T>::push(T const& value)
 	
 	array_[count_] = value;
 	++count_;
-	
-	cond_.notify_all();
 }
 
 template <typename T>
-auto stack<T>::wait_and_pop() -> std::shared_ptr<T>
-{
-	std::unique_lock<std::mutex> lock(mutex_);
-
-	while (empty())
-	{
-		cond_.wait(lock);
-	}
-	--count_;
-	
-	return std::make_shared<T>(array_[count_]);
-}
-
-template <typename T>
-auto stack<T>::try_pop() -> std::shared_ptr<T>
+auto stack<T>::pop() -> std::shared_ptr<T>
 {
 	std::lock_guard<std::mutex> lock(mutex_);
-	
 	if (empty())
 	{
-		return nullptr;
+		throw "Stack is empty! Try again!\n";
 	}
 	else
 	{
 		--count_;
 	}
-	
-	return std::make_shared<T>(array_[count_]);
+	auto top = std::make_shared<T>(array_[count_]);
+	return top;
 }
 
 template <typename T>
