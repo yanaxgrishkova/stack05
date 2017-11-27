e782050560ffd440e8f0e20ef09257752fe62aae
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-11-27 10:44:09 +0300

    Update stack.cpp

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -12,35 +12,12 @@ TEST_CASE("push", "") {
     REQUIRE(s1.count() == 5);
 }
 
-TEST_CASE("try_pop", "") {
+TEST_CASE("pop") 
+{
     stack<int> s1;
-    s1.push(1);
-    s1.push(2);
-    s1.push(3);
-    s1.push(4);
     s1.push(5);
-    REQUIRE(s1.count() == 5);
-    s1.try_pop();
-    REQUIRE(s1.count() == 4);
-    s1.try_pop();
-    REQUIRE(s1.count() == 3);
-    s1.try_pop();
+    s1.push(7);
+    s1.push(9);
+    s1.pop();
     REQUIRE(s1.count() == 2);
 }
-
-TEST_CASE("wait_and_pop", "") {
-    stack<int> s1;
-    s1.push(1);
-    s1.push(2);
-    s1.push(3);
-    s1.push(4);
-    s1.push(5);
-    REQUIRE(s1.count() == 5);
-    s1.wait_and_pop();
-    REQUIRE(s1.count() == 4);
-    s1.wait_and_pop();
-    REQUIRE(s1.count() == 3);
-    s1.wait_and_pop();
-    REQUIRE(s1.count() == 2);
-}
-
